ad2abcad411637b4b242eb833176e5388fcc3305
Mohammad Ibrahim <ibrahimohid77@gmail.com>
2024-12-08 21:49:49 +0600

    project_section_done

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,7 +26,7 @@ const toggle = document.getElementById("toggle");
 
 window.addEventListener("scroll", () => {
     if (window.scrollY > 50) {
-        header.classList.add("py-3", "border-b", "border-b-slate-500");
+        header.classList.add("py-3", "border-b", "border-b-slate-300");
         header.classList.remove("py-8");
         toggle.classList.add("top-3");
         toggle.classList.remove("top-8");
@@ -90,107 +90,287 @@ const displayProjects = (category) => {
             <div class="grid gap-20 md:grid-cols-2 lg:grid-cols-3">
                 <div class="border border-[#77777D33] rounded-md shadow-md w-80">
                     <img src="/assets/images/exener.png" class="w-80 h-52"/>
-                    <div class="pl-5 pb-5">
+                    <div class="px-5 pb-5">
                         <p class="poppins-font text-base text-[#444444] mt-2">Renewable Energy Projects</p>
                         <h3 class="text-lg  text-[#444444] poppins-font my-2">Exener Group</h3>
-
+                        <p class="text-sm font-light">Associated with De Castro Projects | Regenerative Luxury Boutique Hotels</p>
+                        <button class="inline-flex items-center gap-2 rounded-full border border-gray-500 mt-3 px-3 py-1 text-sm text-gray-600 hover:text-white focus:relative hover:bg-[#448c74]">
+                        <a class='tracking-wider' href='https://www.exener.es/' target='_blank'>Live View</a> <svg
+                         xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                         viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                         stroke="currentColor"
+                         class="size-4"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+      />
+      <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+    </svg>
+  </button>
                     </div>
                 </div>
                 <div class="border border-[#77777D33] rounded-md shadow-md w-80">
                     <img src="/assets/images/pilsa.jpeg" class="w-80 h-52"/>
-                    <div class="pl-5 pb-5">
+                    <div class="px-5 pb-5">
                         <p class="poppins-font text-base text-[#444444] mt-2">Educational Initiative</p>
-                        <h3 class="text-lg text-[#444444] poppins-font my-2">Hospitality training and community development program.</h3>
-
+                        <h3 class="text-lg text-[#444444] poppins-font my-2">Ptlsa Hospitaly School</h3>
+                        <p class="text-sm font-light"> Associated with Pilsa Hospitality Solutions (Hotel projects) </p>
+                        <button class="inline-flex items-center gap-2 rounded-full border border-gray-500 mt-3 px-3 py-1 text-sm text-gray-600 hover:text-white focus:relative hover:bg-[#448c74]">
+                        <a class='tracking-wider' href='http://pilsaeduca.com/' target='_blank'>Live View</a> <svg
+                         xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                         viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                         stroke="currentColor"
+                         class="size-4"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+      />
+      <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+    </svg>
+  </button>
                     </div>
                 </div>
                 <div class="border border-[#77777D33] rounded-md shadow-md w-80">
                     <img src="/assets/images/Young_Talent_Social_Project.png" class="w-80 h-52"/>
-                    <div class="pl-5 pb-5">
+                    <div class="px-5 pb-5">
                         <p class="poppins-font text-base text-[#444444] mt-2">Web Application</p>
-                        <h3 class="text-lg text-[#444444] poppins-font my-2 font-medium">Social project promoting young talent through technological and educational platforms.</h3>
-
+                        <h3 class="text-lg text-[#444444] poppins-font my-2 font-medium">Global Educational Platforms.</h3>
+                                         <p class="text-sm font-light">Commercial and Marketing Director.
+Social project to promote young talent.
+</p>
+                        <button class="inline-flex items-center gap-2 rounded-full border border-gray-500 mt-3 px-3 py-1 text-sm text-gray-600 hover:text-white focus:relative hover:bg-[#448c74]">
+                        <a class='tracking-wider' href='http://www.slideshare.net/GlobalIndevices/dossier-prensa-mwc15-v6
+' target='_blank'>Live View</a> <svg
+                         xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                         viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                         stroke="currentColor"
+                         class="size-4"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+      />
+      <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+    </svg>
+  </button>
                     </div>
                 </div>
                 <div class="border border-[#77777D33] rounded-md shadow-md w-80">
                     <img src="/assets/images/Medicaluxe.webp" class="w-80 h-52"/>
-                    <div class="pl-5 pb-5">
+                    <div class="px-5 pb-5">
                         <p class="poppins-font text-base text-[#444444] mt-2">Medicaluxe</p>
-                        <h3 class="text-lg text-[#444444] poppins-font my-2 font-medium">Focused on SPAs and medical clinics.</h3>
-
+                        <h3 class="text-lg text-[#444444] poppins-font my-2 font-medium">Beauty & Health Care</h3>
+                                         <p class="text-sm font-light">Distribución de productos dermoestéticos de lujo.</p>
+                        <button class="inline-flex items-center gap-2 rounded-full border border-gray-500 mt-3 px-3 py-1 text-sm text-gray-600 hover:text-white focus:relative hover:bg-[#448c74]">
+                        <a class='tracking-wider' href='http://www.medicaluxe.com/
+' target='_blank'>Live View</a> <svg
+                         xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                         viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                         stroke="currentColor"
+                         class="size-4"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+      />
+      <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+    </svg>
+  </button>
                     </div>
                 </div>
                 <div class="border border-[#77777D33] rounded-md shadow-md w-80">
                     <img src="/assets/images/Webinar.jpeg" class="w-80 h-52"/>
-                    <div class="pl-5 pb-5">
+                    <div class="px-5 pb-5">
                         <p class="poppins-font text-base text-[#444444] mt-2">Knowledge Sharing / Webinar</p>
-                        <h3 class="text-lg text-[#444444] poppins-font my-2 font-medium">Educational and industry insights. Ecotourism and sustainable luxury in a post-pandemic world.</h3>
 
+<h3 class="text-lg text-[#444444] poppins-font my-2 font-medium">Knowledge is Power!</h3>
+                                         <p class="text-sm font-light">Commercial and Marketing Director.
+Social project to promote young talent.
+</p>
+                        <button class="inline-flex items-center gap-2 rounded-full border border-gray-500 mt-3 px-3 py-1 text-sm text-gray-600 hover:text-white focus:relative hover:bg-[#448c74]">
+                        <a class='tracking-wider' href='http://www.slideshare.net/GlobalIndevices/dossier-prensa-mwc15-v6' target='_blank'>Live View</a> <svg
+                         xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                         viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                         stroke="currentColor"
+                         class="size-4"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+      />
+      <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+    </svg>
+  </button>
                     </div>
                 </div>
             </div>
         `;
+<<<<<<< HEAD
+    } else if (category === 'webApp') {
+=======
     }
     else if (category === 'webApp') {
-
         projectItem.innerHTML = `
             <div class="grid gap-20 md:grid-cols-2 lg:grid-cols-3">
-                 <div class="border border-[#77777D33] rounded-md shadow-md w-80">
+                  <div class="border border-[#77777D33] rounded-md shadow-md w-80">
                     <img src="/assets/images/Young_Talent_Social_Project.png" class="w-80 h-52"/>
-                    <div class="pl-5 pb-5">
+                    <div class="px-5 pb-5">
                         <p class="poppins-font text-base text-[#444444] mt-2">Web Application</p>
-                        <h3 class="text-lg text-[#444444] poppins-font my-2 font-medium">Social project promoting young talent through technological and educational platforms.</h3>
-
+                        <h3 class="text-lg text-[#444444] poppins-font my-2 font-medium">Global Educational Platforms.</h3>
+                                         <p class="text-sm font-light">Commercial and Marketing Director.
+Social project to promote young talent.
+</p>
+                        <button class="inline-flex items-center gap-2 rounded-full border border-gray-500 mt-3 px-3 py-1 text-sm text-gray-600 hover:text-white focus:relative hover:bg-[#448c74]">
+                        <a class='tracking-wider' href='http://www.slideshare.net/GlobalIndevices/dossier-prensa-mwc15-v6' target='_blank'>Live View</a> <svg
+                         xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                         viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                         stroke="currentColor"
+                         class="size-4"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+      />
+      <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+    </svg>
+  </button>
                     </div>
                 </div>
-                <div class="border border-[#77777D33] rounded-md shadow-md w-80">
+               <div class="border border-[#77777D33] rounded-md shadow-md w-80">
                     <img src="/assets/images/Medicaluxe.webp" class="w-80 h-52"/>
-                    <div class="pl-5 pb-5">
+                    <div class="px-5 pb-5">
                         <p class="poppins-font text-base text-[#444444] mt-2">Medicaluxe</p>
-                        <h3 class="text-lg text-[#444444] poppins-font my-2 font-medium">Focused on SPAs and medical clinics.</h3>
-
+                        <h3 class="text-lg text-[#444444] poppins-font my-2 font-medium">Beauty & Health Care</h3>
+                                         <p class="text-sm font-light">Distribución de productos dermoestéticos de lujo.</p>
+                        <button class="inline-flex items-center gap-2 rounded-full border border-gray-500 mt-3 px-3 py-1 text-sm text-gray-600 hover:text-white focus:relative hover:bg-[#448c74]">
+                        <a class='tracking-wider' href='http://www.medicaluxe.com/
+' target='_blank'>Live View</a> <svg
+                         xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                         viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                         stroke="currentColor"
+                         class="size-4"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+      />
+      <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+    </svg>
+  </button>
                     </div>
                 </div>
             </div>
         `;
-    }
-    else if (category === 'webinar') {
+    } else if (category === 'webinar') {
         projectItem.innerHTML = `
             <div class="grid gap-20 md:grid-cols-2 lg:grid-cols-3">
                  <div class="border border-[#77777D33] rounded-md shadow-md w-80">
                     <img src="/assets/images/Webinar.jpeg" class="w-80 h-52"/>
-                    <div class="pl-5 pb-5">
+                    <div class="px-5 pb-5">
                         <p class="poppins-font text-base text-[#444444] mt-2">Knowledge Sharing / Webinar</p>
-                        <h3 class="text-lg text-[#444444] poppins-font my-2 font-medium">Educational and industry insights. Ecotourism and sustainable luxury in a post-pandemic world.</h3>
 
+<h3 class="text-lg text-[#444444] poppins-font my-2 font-medium">Knowledge is Power!</h3>
+                                         <p class="text-sm font-light">Commercial and Marketing Director.
+Social project to promote young talent.
+</p>
+                        <button class="inline-flex items-center gap-2 rounded-full border border-gray-500 mt-3 px-3 py-1 text-sm text-gray-600 hover:text-white focus:relative hover:bg-[#448c74]">
+                        <a class='tracking-wider' href='http://www.slideshare.net/GlobalIndevices/dossier-prensa-mwc15-v6' target='_blank'>Live View</a> <svg
+                         xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                         viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                         stroke="currentColor"
+                         class="size-4"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+      />
+      <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+    </svg>
+  </button>
                     </div>
                 </div>
             </div>
         `;
-    }
-    else if (category === 'educationalInitiative') {
+    } else if (category === 'educationalInitiative') {
         projectItem.innerHTML = `
-            <div class="grid gap-20 md:grid-cols-2 lg:grid-cols-3">
-                <div class="border border-[#77777D33] rounded-md shadow-md w-80">
+             <div class="border border-[#77777D33] rounded-md shadow-md w-80">
                     <img src="/assets/images/pilsa.jpeg" class="w-80 h-52"/>
-                    <div class="pl-5 pb-5">
+                    <div class="px-5 pb-5">
                         <p class="poppins-font text-base text-[#444444] mt-2">Educational Initiative</p>
-                        <h3 class="text-lg text-[#444444] poppins-font my-2">Hospitality training and community development program.</h3>
-
+                        <h3 class="text-lg text-[#444444] poppins-font my-2">PILSA HOSPITALITY SCHOOL</h3>
+                        <p class="text-sm"> Associated with Pilsa Hospitality Solutions (Hotel projects) </p>
+                        <button class="inline-flex items-center gap-2 rounded-full border border-gray-500 mt-3 px-3 py-1 text-sm text-gray-600 hover:text-white focus:relative hover:bg-[#448c74]">
+                        <a class='tracking-wider' href='http://pilsaeduca.com/' target='_blank'>Live View</a> <svg
+                         xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                         viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                         stroke="currentColor"
+                         class="size-4"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+      />
+      <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+    </svg>
+  </button>
                     </div>
                 </div>
-            </div>
         `;
-    }
-    else if (category === 'renewableEnergyProjects') {
+    } else if (category === 'renewableEnergyProjects') {
         projectItem.innerHTML = `
             <div class="grid gap-20 md:grid-cols-2 lg:grid-cols-3">
-                 <div class="border border-[#77777D33] rounded-md shadow-md w-80">
+                  <div class="border border-[#77777D33] rounded-md shadow-md w-80">
                     <img src="/assets/images/exener.png" class="w-80 h-52"/>
-                    <div class="pl-5 pb-5">
+                    <div class="px-5 pb-5">
                         <p class="poppins-font text-base text-[#444444] mt-2">Renewable Energy Projects</p>
                         <h3 class="text-lg  text-[#444444] poppins-font my-2">Exener Group</h3>
-
+                        <p class="text-sm font-light">Associated with De Castro Projects | Regenerative Luxury Boutique Hotels</p>
+                        <button class="inline-flex items-center gap-2 rounded-full border border-gray-500 mt-3 px-3 py-1 text-sm text-gray-600 hover:text-white focus:relative hover:bg-[#448c74]">
+                        <a class='tracking-wider' href='https://www.exener.es/' target='_blank'>Live View</a> <svg
+                         xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                         viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                         stroke="currentColor"
+                         class="size-4"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+      />
+      <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+    </svg>
+  </button>
                     </div>
                 </div>
             </div>
